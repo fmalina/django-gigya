92796201c24eb3b21cc220a85171c34500dc9e55
Frantisek Malina <fmalina@gmail.com>
2016-11-17 11:04:22 +0000

    Added default API domain setting

--- gigya/app_settings.py
+++ gigya/app_settings.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 GIGYA_API_KEY = getattr(settings, 'GIGYA_API_KEY', 'not set')
 GIGYA_SECRET_KEY = getattr(settings, 'GIGYA_SECRET_KEY', 'not set')
+GIGYA_API_DOMAIN = getattr(settings, 'GIGYA_API_DOMAIN', 'us1.gigya.com')
