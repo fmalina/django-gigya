75c35b55877764f8c4e8c3e5c9226fb15bf86c88
Frantisek Malina <fmalina@gmail.com>
2016-11-17 11:08:27 +0000

    Added email to register form

--- gigya/forms.py
+++ gigya/forms.py
@@ -4,6 +4,7 @@ from django import forms
 class RegistrationForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100)
     last_name = forms.CharField(label='Last Name', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     confirm_password = forms.CharField(
         label='Confirm Password',
