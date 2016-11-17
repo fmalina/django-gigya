b7632f5643cf62c253c653ee203290f12c1e71dc
Frantisek Malina <fmalina@gmail.com>
2016-11-17 10:35:19 +0000

    Added backend test

--- gigya/app_settings.py
+++ gigya/app_settings.py
@@ -1,5 +1,4 @@
 from django.conf import settings
 
-GIGYA_API_KEY = getattr(settings, 'GIGYA_API_KEY', 'foo-bar')
-
-
+GIGYA_API_KEY = getattr(settings, 'GIGYA_API_KEY', 'not set')
+GIGYA_SECRET_KEY = getattr(settings, 'GIGYA_SECRET_KEY', 'not set')
